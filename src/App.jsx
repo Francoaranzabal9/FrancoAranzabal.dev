dbe30d7756b0766072088d9f1b4a0925e334bc45
francoaranzabal9 <juanfrancoaranzabal@gmail.com>
2025-11-05 17:58:27 -0300

    add: logica de toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home.jsx"
 import { NotFound } from "./pages/NotFound.jsx"
+import { ToastProvider } from "./components/ui/toast.jsx"
+import { Toaster } from "./components/ui/toaster.jsx"
 
 
 
 function App() {
 
-
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <ToastProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <Toaster />
+      </ToastProvider>
     </>
   )
 }
